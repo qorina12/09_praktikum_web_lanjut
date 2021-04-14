ada878057ec95b44d8cf5e574aa4dd0c3cdce699
qorina12 <prameswarides12@gmail.com>
2021-04-14 21:42:01 +0700

    modifikasi editblade

--- resources/views/mahasiswas/edit.blade.php
+++ resources/views/mahasiswas/edit.blade.php
@@ -32,8 +32,12 @@
                 <input type="text" name="Nama" class="form-control" id="Nama" value="{{ $Mahasiswa->Nama }}" aria-describedby="Nama" > 
             </div>
             <div class="form-group">
-                <label for="Kelas">Kelas</label> 
-                <input type="Kelas" name="Kelas" class="form-control" id="Kelas" value="{{ $Mahasiswa->Kelas }}" aria-describedby="Kelas" > 
+                <label for="Kelas">Kelas</label>
+                <select name="Kelas" id="Kelas" class="form-select">
+                @foreach($kelas as $kls)
+                    <option value="{{ $kls->id }}" {{ $Mahasiswa->kelas_id == $kls->id ? 'selected': ''}}>{{ $kls->nama_kelas }}</option>
+                @endforeach
+                </select>
             </div>
             <div class="form-group">
                 <label for="Jurusan">Jurusan</label> 
